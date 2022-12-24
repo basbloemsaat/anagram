1d64f2352c4e642489ce23c265f4cf34a7794e01
Bas Bloemsaat <bas@bloemsaat.com>
2022-12-24 22:19:07 +0100

    build works

--- source/ts/index.ts
+++ source/ts/index.ts
@@ -1,161 +1,168 @@
 import "../css/main.scss";
 import * as d3 from "d3";
 
-const svg = d3.select("svg");
+function LOADED() {
+  const svg = d3.select("svg");
 
-let width = parseInt(svg.style("width"));
-let height = parseInt(svg.style("height"));
-const scaleLin = d3.scaleLinear().range([50, width - 50]);
-const g = svg
-  .append("g")
-  .classed("graph", true)
-  .attr("transform", "translate(0,200)");
+  let width = parseInt(svg.style("width"));
+  let height = parseInt(svg.style("height"));
+  const scaleLin = d3.scaleLinear().range([50, width - 50]);
+  const g = svg
+    .append("g")
+    .classed("graph", true)
+    .attr("transform", "translate(0,200)");
 
-interface Node {
-  letter: string;
-  id: number;
-  end?: boolean;
-}
-interface Chain {
-  nodes: Node[];
-}
-
-let graph: Chain;
-
-function textToGraph(t: string): Chain {
-  const text_array = t.split("");
-  const graph: Chain = {
-    nodes: text_array.map((e: string, i: number) => {
-      return { letter: e, id: i, fy: 0 };
-    }),
-  };
-
-  let x = Array.from(Array(graph.nodes.length).keys()).map((d) => "" + d);
-  scaleLin.domain([0, graph.nodes.length - 1]);
-
-  return graph;
-}
-
-function shuffle(array: Node[]) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+  interface Node {
+    letter: string;
+    id: number;
+    end?: boolean;
+  }
+  interface Chain {
+    nodes: Node[];
   }
 
-  return array;
-}
+  let graph: Chain;
 
-function randomizeNodes() {
-  shuffle(graph.nodes);
-  reidNodes(graph);
-  g.selectAll("g.node")
-    .transition()
-    .duration(200)
-    .attr("transform", (d: Node) => `translate(${scaleLin(d.id)},0)`);
-}
+  function textToGraph(t: string): Chain {
+    const text_array = t.split("");
+    const graph: Chain = {
+      nodes: text_array.map((e: string, i: number) => {
+        return { letter: e, id: i, fy: 0 };
+      }),
+    };
 
-function reidNodes(graph: Chain): void {
-  graph.nodes.forEach((e, i) => {
-    e.id = i;
-  });
-}
+    let x = Array.from(Array(graph.nodes.length).keys()).map((d) => "" + d);
+    scaleLin.domain([0, graph.nodes.length - 1]);
 
-function clamp(x: number, lo: number, hi: number) {
-  return x < lo ? lo : x > hi ? hi : x;
-}
+    return graph;
+  }
 
-let dragnode: Node;
+  function shuffle(array: Node[]) {
+    let currentIndex = array.length,
+      randomIndex;
 
-function dragstart(event: MouseEvent, d: Node) {
-  dragnode = graph.nodes[d.id];
-}
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-function dragged(event: MouseEvent, d: Node) {
-  let x = clamp(event.x, 0, width);
-  d3.select(this).attr("transform", (d) => `translate(${event.x},0)`);
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
 
-  let new_id = Math.round(scaleLin.invert(x));
+    return array;
+  }
 
-  if (new_id != d.id) {
-    let oldpos = d.id;
-    let node = graph.nodes[oldpos];
-
-    let newpos = new_id;
-    graph.nodes.splice(oldpos, 1);
-    graph.nodes.splice(newpos, 0, node);
+  function randomizeNodes() {
+    shuffle(graph.nodes);
     reidNodes(graph);
     g.selectAll("g.node")
-      .filter((d: Node) => d.id != node.id)
       .transition()
       .duration(200)
       .attr("transform", (d: Node) => `translate(${scaleLin(d.id)},0)`);
-
-    // .attr("cx", (d: Node, i) => scaleLin(d.id));
   }
-}
 
-function dragend(event: MouseEvent, d: Node) {
-  g.selectAll("g.node").attr(
-    "transform",
-    (d: Node) => `translate(${scaleLin(d.id)},0)`
-  );
-}
+  function reidNodes(graph: Chain): void {
+    graph.nodes.forEach((e, i) => {
+      e.id = i;
+    });
+  }
 
-const drag = d3
-  .drag()
-  .on("start", dragstart)
-  .on("drag", dragged)
-  .on("end", dragend);
+  function clamp(x: number, lo: number, hi: number) {
+    return x < lo ? lo : x > hi ? hi : x;
+  }
 
-const redraw = () => {
-  // @ts-expect-error
-  graph = textToGraph(d3.select("input").node().value);
+  let dragnode: Node;
 
-  const node = g
-    .selectAll("g.node")
-    .data(graph.nodes)
-    .join("g")
-    .classed("node", true)
-    .classed("end", (d: Node) => !!d.end)
-    .attr("transform", (d) => `translate(${scaleLin(d.id)},0)`);
+  function dragstart(event: MouseEvent, d: Node) {
+    dragnode = graph.nodes[d.id];
+  }
 
-  node
-    .append("circle")
-    // .attr("cx", (d, i) => scaleLin(d.id))
-    .classed("backg", true);
+  function dragged(event: MouseEvent, d: Node) {
+    let x = clamp(event.x, 0, width);
+    d3.select(this).attr("transform", (d) => `translate(${event.x},0)`);
 
-  node
-    .append("text")
-    // .attr("x", (d, i) => scaleLin(d.id))
-    .text((d) => d.letter);
+    let new_id = Math.round(scaleLin.invert(x));
 
-  node
-    .append("circle")
-    // .attr("cx", (d, i) => scaleLin(d.id))
-    .classed("overlay", true);
+    if (new_id != d.id) {
+      let oldpos = d.id;
+      let node = graph.nodes[oldpos];
 
-  g.selectAll(".node").call(drag);
-};
+      let newpos = new_id;
+      graph.nodes.splice(oldpos, 1);
+      graph.nodes.splice(newpos, 0, node);
+      reidNodes(graph);
+      g.selectAll("g.node")
+        .filter((d: Node) => d.id != node.id)
+        .transition()
+        .duration(200)
+        .attr("transform", (d: Node) => `translate(${scaleLin(d.id)},0)`);
 
-redraw();
+      // .attr("cx", (d: Node, i) => scaleLin(d.id));
+    }
+  }
 
-d3.select("input").on("keyup", () => {
+  function dragend(event: MouseEvent, d: Node) {
+    g.selectAll("g.node").attr(
+      "transform",
+      (d: Node) => `translate(${scaleLin(d.id)},0)`
+    );
+  }
+
+  const drag = d3
+    .drag()
+    .on("start", dragstart)
+    .on("drag", dragged)
+    .on("end", dragend);
+
+  const redraw = () => {
+    // @ts-expect-error
+    graph = textToGraph(d3.select("input").node().value);
+
+    const node = g
+      .selectAll("g.node")
+      .data(graph.nodes)
+      .join("g")
+      .classed("node", true)
+      .classed("end", (d: Node) => !!d.end)
+      .attr("transform", (d) => `translate(${scaleLin(d.id)},0)`);
+
+    node
+      .append("circle")
+      // .attr("cx", (d, i) => scaleLin(d.id))
+      .classed("backg", true);
+
+    node
+      .append("text")
+      // .attr("x", (d, i) => scaleLin(d.id))
+      .text((d) => d.letter);
+
+    node
+      .append("circle")
+      // .attr("cx", (d, i) => scaleLin(d.id))
+      .classed("overlay", true);
+
+    g.selectAll(".node").call(drag);
+  };
+
   redraw();
-});
 
-d3.select("button#randomizer").on("click", (e: Event) => {
-  randomizeNodes();
-  e.preventDefault();
-  return false;
-});
+  d3.select("input").on("keyup", () => {
+    redraw();
+  });
+
+  d3.select("button#randomizer").on("click", (e: Event) => {
+    randomizeNodes();
+    e.preventDefault();
+    return false;
+  });
+}
+
+window.onload = () => {
+  console.log("ONLOAD");
+  LOADED();
+};
